ad80c0208b1b6e1e4d18aa538038b76853fbf333
0________4 <you@example.com>
2020-02-29 00:55:19 +0300

    Add Job class and other updates

---
+++ app/src/main/java/com/fortie40/notificationscheduler/jobs/ShowNotification.kt
@@ -0,0 +1,64 @@
+package com.fortie40.notificationscheduler.jobs
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import androidx.core.app.NotificationCompat
+import com.fortie40.notificationscheduler.MainActivity
+import com.fortie40.notificationscheduler.R
+
+class ShowNotification() {
+    companion object {
+        const val PRIMARY_CHANNEL_ID =
+            "com.fortie40.notificationscheduler.PRIMARY_NOTIFICATION_CHANNEL"
+        const val NOTIFICATION_ID = 0
+    }
+
+    private lateinit var mNotifyManager: NotificationManager
+    private lateinit var mContext: Context
+
+    constructor(notifyManager: NotificationManager, context: Context): this() {
+        mNotifyManager = notifyManager
+        mContext = context
+    }
+
+    fun createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(PRIMARY_CHANNEL_ID,
+                mContext.getString(R.string.job_service_notification),
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = mContext.getString(R.string.notifications_from_job_service)
+            }
+
+            mNotifyManager.createNotificationChannel(channel)
+
+        }
+    }
+
+    private fun getNotificationBuilder(): NotificationCompat.Builder {
+        val notificationIntent = Intent(mContext, MainActivity::class.java)
+        val notificationPendingIntent = PendingIntent.getActivity(mContext,
+            NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        return NotificationCompat.Builder(mContext, PRIMARY_CHANNEL_ID)
+            .setContentTitle(mContext.getString(R.string.job_service))
+            .setContentText(mContext.getString(R.string.your_job_is_running))
+            .setSmallIcon(R.drawable.ic_job_running)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setContentIntent(notificationPendingIntent)
+            .setAutoCancel(true)
+    }
+
+    fun sendNotifications() {
+        val notifyBuilder = getNotificationBuilder()
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build())
+
+    }
+}
--- app/src/main/java/com/fortie40/notificationscheduler/jobschedulers/NotificationJobService.kt
+++ app/src/main/java/com/fortie40/notificationscheduler/jobschedulers/NotificationJobService.kt
@@ -1,15 +1,24 @@
 package com.fortie40.notificationscheduler.jobschedulers
 
+import android.app.NotificationManager
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Context
+import com.fortie40.notificationscheduler.jobs.ShowNotification
 
 class NotificationJobService: JobService() {
+    private val notifyManager = getSystemService(Context.NOTIFICATION_SERVICE)
+            as NotificationManager
 
     override fun onStopJob(p0: JobParameters?): Boolean {
         return true
     }
 
     override fun onStartJob(p0: JobParameters?): Boolean {
+        val showNotification = ShowNotification(notifyManager, this)
+        showNotification.createNotificationChannel()
+        showNotification.sendNotifications()
+        //return true
         return false
     }
 }
