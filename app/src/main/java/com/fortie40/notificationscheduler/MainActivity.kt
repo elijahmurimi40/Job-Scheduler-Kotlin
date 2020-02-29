8307a4c2be73c4eeefe7a55c59408c906b34484d
0________4 <you@example.com>
2020-02-29 22:32:09 +0300

    Override Deadline Constraint

--- app/src/main/java/com/fortie40/notificationscheduler/MainActivity.kt
+++ app/src/main/java/com/fortie40/notificationscheduler/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
 import android.os.Bundle
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fortie40.notificationscheduler.jobschedulers.NotificationJobService
@@ -30,12 +31,28 @@ class MainActivity : AppCompatActivity() {
         cancel_job.setOnClickListener {
             cancelJob()
         }
+
+        seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if (p1 > 0) {
+                    seek_bar_progress.text = getString(R.string.seconds, p1.toString())
+                } else {
+                    seek_bar_progress.text = getString(R.string.not_set)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 
     private fun scheduleJob() {
         mScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         val selectedNetworkId = networkOptions.checkedRadioButtonId
         var selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
+        val seekBarInteger = seek_bar.progress
+        val seekBarSet = seekBarInteger > 0
 
         when(selectedNetworkId) {
             R.id.no_network -> selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
@@ -50,7 +67,11 @@ class MainActivity : AppCompatActivity() {
             .setRequiresCharging(charging_switch.isChecked)
 
         val constraintSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE ||
-                charging_switch.isChecked || idle_switch.isChecked
+                charging_switch.isChecked || idle_switch.isChecked || seekBarSet
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger.toLong() * 1000)
+        }
 
         if (constraintSet) {
             val myJobInfo = builder.build()
